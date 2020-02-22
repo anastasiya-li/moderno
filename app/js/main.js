b1e6f5536d3aa395235036b800ba9f36ca3d5590
anastasiya-li <nastya.lizun15@gmail.com>
2020-02-22 14:43:49 +0500

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,14 @@ $('.icon-th-large').on('click',function(){
     $('.icon-th-large').addClass('active');
 });
 
+$('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+});
+
+$('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+});
+
 var mixer = mixitup('.products__inner-box');
 
     
